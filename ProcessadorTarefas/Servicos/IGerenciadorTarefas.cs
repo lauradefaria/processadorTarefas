f39ccc959efb5e4449770a115df28ee573587467
Laura de Faria Maranhão Ayres <lauradfma@gmail.com>
2024-02-19 05:13:17 -0300

    Update IGerenciadorTarefas.cs

--- ProcessadorTarefas/Servicos/IGerenciadorTarefas.cs
+++ ProcessadorTarefas/Servicos/IGerenciadorTarefas.cs
@@ -1,4 +1,4 @@
-﻿using ProcessadorTarefas.Entidades;
+using ProcessadorTarefas.Entidades;
 
 namespace ProcessadorTarefas.Servicos
 {
@@ -9,5 +9,70 @@ namespace ProcessadorTarefas.Servicos
         Task Cancelar(int idTarefa);
         Task<IEnumerable<Tarefa>> ListarAtivas();
         Task<IEnumerable<Tarefa>> ListarInativas();
+    }
+
+    public class Gerenciador : IGerenciadorTarefas
+    {
+
+        private readonly IRepository<Tarefa> _tarefaRepository;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IConfiguration _configs;
+        private static Random random = new Random();
+
+        private readonly EstadoTarefa[] estadosAtivos = [
+                    EstadoTarefa.Criada,
+            EstadoTarefa.EmExecucao,
+            EstadoTarefa.Agendada,
+            EstadoTarefa.EmPausa
+                ];
+        private readonly EstadoTarefa[] estadosInativos = [EstadoTarefa.Cancelada, EstadoTarefa.Concluida];
+
+        public Gerenciador(IServiceProvider serviceProvider, IRepository<Tarefa> tarefaRepository, IConfiguration configs)
+        {
+            _tarefaRepository = tarefaRepository;
+            _serviceProvider = serviceProvider;
+            _configs = configs;
+        }
+
+        public async Task Cancelar(int idTarefa)
+        {
+            IProcessadorTarefas? processador = _serviceProvider.GetService(typeof(IProcessadorTarefas)) as IProcessadorTarefas;
+
+            if (processador != null)
+                await processador.CancelarTarefa(idTarefa);
+            else
+                throw new Exception("Não foi possível acessar o processador de tarefas.");
+        }
+
+        public async Task<Tarefa?> Consultar(int idTarefa)
+            => await Task.FromResult(_tarefaRepository.GetById(idTarefa));
+
+        public async Task<Tarefa> Criar()
+        {
+            var ultimaTarefa = _tarefaRepository.GetAll().MaxBy(tarefa => tarefa.Id);
+
+            var novaTarefa = Tarefa.Criar(ultimaTarefa?.Id + 1 ?? 1, _configs);
+
+            _tarefaRepository.Add(novaTarefa);
+            return await Task.FromResult(novaTarefa);
+        }
+
+        public async Task<IEnumerable<Tarefa>> ListarAtivas()
+        {
+            return await Task.FromResult(
+                _tarefaRepository
+                    .GetAll()
+                    .Where(t => estadosAtivos.Contains(t.Estado))
+                );
+        }
+
+        public async Task<IEnumerable<Tarefa>> ListarInativas()
+        {
+            return await Task.FromResult(
+                _tarefaRepository
+                    .GetAll()
+                    .Where(t => estadosInativos.Contains(t.Estado))
+                );
+        }
     }
 }
